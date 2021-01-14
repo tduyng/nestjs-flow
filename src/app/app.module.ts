669cc1d101056c74960ec3703372bc5a127ff322
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-14 11:16:34 +0100

    Update Post module in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
+import { PostModule } from '@modules/post/post.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
