52f78c72be25f9e065383bd86e20004b7b199050
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-15 00:43:37 +0100

    Using variable env for jwt expire time

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '2h' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION_TIME },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
