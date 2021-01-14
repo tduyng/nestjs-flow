9db18481ce20262de4b025f5c912b6c99ca16059
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-14 23:28:22 +0100

    Using Jwtguard in Post controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -6,10 +6,12 @@ import {
   Put,
   Delete,
   Param,
+  UseGuards,
 } from '@nestjs/common';
 import { CreatePostDto, UpdatePostDto } from './dto';
 import { PostService } from './post.service';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@modules/auth/guards/jwt-auth.guard';
 
 @ApiTags('Posts')
 @Controller('posts')
@@ -26,11 +28,13 @@ export class PostController {
     return await this.postService.getPostById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('/')
   public async createPost(@Body() postDto: CreatePostDto) {
     return await this.postService.createPost(postDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put('/:id')
   public async updatePost(
     @Param('id') id: string,
@@ -39,6 +43,7 @@ export class PostController {
     return await this.postService.updatePost(id, postDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   public async deletePost(@Param('id') id: string) {
     return await this.postService.deletePost(id);
