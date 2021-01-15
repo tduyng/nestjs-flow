1298552d6f273ae197442cb4009e02ab72b10dbb
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-15 09:51:19 +0100

    Update validation in user entity

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,4 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDate, IsEmail, Min } from 'class-validator';
+import moment from 'moment';
 
 @Entity()
 export class User {
@@ -9,8 +11,26 @@ export class User {
   name: string;
 
   @Column({ unique: true })
+  @IsEmail()
   email: string;
 
   @Column()
+  @Min(0)
   password: string;
+
+  @Column({
+    type: Date,
+    default: moment(new Date()).format('YYYY-MM-DD HH:ss'),
+    nullable: true,
+  })
+  @IsDate()
+  createdAt: Date;
+
+  @Column({
+    type: Date,
+    default: moment(new Date()).format('YYYY-MM-DD HH:ss'),
+    nullable: true,
+  })
+  @IsDate()
+  updatedAt: Date;
 }
