2e708c6c107ae5c07aad61032b101a5f1c623a27
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-16 17:28:12 +0100

    Integrate PostRepository in PostModule

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
-import { Post } from './post.entity';
+import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([PostRepository])],
   controllers: [PostController],
   providers: [PostService],
 })
