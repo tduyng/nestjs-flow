ed45dcfe13195872b75436ab0bfc3156bc198fa6
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-16 17:31:28 +0100

    Add test for AppService

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    appService = app.get<AppService>(AppService);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appService.getHello()).toBe('Hello World!');
+    });
+  });
+});
