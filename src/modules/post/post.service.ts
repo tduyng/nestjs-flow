95d59178f69038ec3e447c7d1aa6f2ce992224e5
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-16 18:32:01 +0100

    Fix eror

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { CreatePostDto, UpdatePostDto } from './dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Post } from './post.entity';
 import { PostRepository } from './post.repository';
 
