a668ceb2658f3d78801d92db04cdc8df1f187ac4
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-18 14:31:23 +0100

    Fix error not trigger env variable

--- src/common/config/ormConfig.ts
+++ src/common/config/ormConfig.ts
@@ -4,45 +4,49 @@ export function ormConfig(): any {
   const envMode = process.env.NODE_ENV || 'development';
   switch (envMode) {
     case 'development':
-      return configForDevelopment;
+      return configForDevelopment();
     case 'test':
-      return configForTesting;
+      return configForTesting();
     case 'production':
-      return configForProduction;
+      return configForProduction();
     default:
-      return configForDevelopment;
+      return configForDevelopment();
   }
 }
 
-const configForTesting = {
-  type: 'sqlite',
-  database: 'src/common/databases/test.db',
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  synchronize: true,
+const configForTesting = () => {
+  return {
+    type: 'sqlite',
+    database: 'src/common/databases/test.db',
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    synchronize: true,
+  };
 };
-
-const configForDevelopment = {
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: Number(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  autoLoadEntities: true,
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  logging: false,
-  synchronize: true,
+const configForDevelopment = () => {
+  return {
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    autoLoadEntities: true,
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    logging: false,
+    synchronize: true,
+  };
 };
-
-const configForProduction = {
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: Number(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  autoLoadEntities: true,
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  logging: false,
-  synchronize: true,
+const configForProduction = () => {
+  return {
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    autoLoadEntities: true,
+    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    logging: false,
+    synchronize: true,
+  };
 };
