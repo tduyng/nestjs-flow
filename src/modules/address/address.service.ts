5bff16a0340a6097aa6237e8b8673d9436047d38
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-19 19:14:49 +0100

    Create AddressService

---
+++ src/modules/address/address.service.ts
@@ -0,0 +1,69 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Address } from './address.entity';
+import { AddressRepository } from './address.repository';
+import { CreateAddressDto, UpdateAddressDto } from './dto';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    @InjectRepository(Address)
+    private readonly addressRepo: AddressRepository,
+  ) {}
+
+  public async getAddressById(id: string) {
+    try {
+      const address = await this.addressRepo.getAddressById(id);
+      if (!address) {
+        throw new NotFoundException('Address not found');
+      }
+      return address;
+    } catch (error) {
+      if (error.status === HttpStatus.NOT_FOUND) {
+        throw error;
+      } else {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  public async createAddress(addressDto: CreateAddressDto) {
+    try {
+      return await this.addressRepo.createAddress(addressDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public async updateAddress(id: string, addressDto: UpdateAddressDto) {
+    try {
+      const address = await this.getAddressById(id);
+      return await this.addressRepo.updateAddress(address, addressDto);
+    } catch (error) {
+      if (error.status === HttpStatus.NOT_FOUND) {
+        throw error;
+      } else {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  public async deleteAddress(id: string) {
+    try {
+      return this.addressRepo.deleteAddress(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
