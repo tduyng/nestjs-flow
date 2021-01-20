a536a0efed2d4a082ea6d4c1c845419bd14015c7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-20 18:57:36 +0100

    Update test for addressService

--- src/modules/files/public-file.entity.ts
+++ src/modules/files/public-file.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class PublicFile {
