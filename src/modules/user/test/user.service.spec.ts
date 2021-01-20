0d8ef633e8253b42a794dc7c78243fa027d83b14
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-01-20 19:56:47 +0100

    Update test for UserService

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,28 +1,77 @@
 import { AddressService } from '@modules/address/address.service';
+import { PublicFile } from '@modules/files/public-file.entity';
 import { FilesService } from '@modules/files/services/files.service';
 import { NotFoundException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserRepository } from '../user.repository';
 import { UserService } from '../user.service';
+import { User } from '../user.entity';
+import { CreateAddressDto } from '@modules/address/dto';
+import { Address } from '@modules/address/address.entity';
+
+const oneUser = {
+  id: 'some id',
+  email: 'some email',
+  password: 'some password',
+  avatar: null,
+  address: null,
+} as User;
+const oneUserWithAvatar = {
+  id: 'some id',
+  email: 'some email',
+  password: 'some password',
+  address: null,
+  avatar: {
+    id: 'some id',
+    key: 'some key',
+    url: 'some url',
+  },
+} as User;
+
+const oneAvatar = {
+  id: 'some id',
+  key: 'some key',
+  url: 'some url',
+} as PublicFile;
+
+const oneAddress = {
+  id: 'some id',
+  number: 'some number',
+  street: 'some street',
+  city: 'some city',
+  country: 'some country',
+} as Address;
 
 describe('UserService', () => {
   let userService: UserService;
   let userRepository;
+  let filesService;
+  let addressService;
 
   const mockUserRepository = () => ({
     getUsers: jest.fn(),
     getUserById: jest.fn(),
     getUserByEmail: jest.fn(),
     getUserByIdOrEmail: jest.fn(),
+    deleteUser: jest.fn(),
     updateAvatar: jest.fn(),
-    deleteAvatar: jest.fn(),
+    createAddress: jest.fn(),
+    updateAddress: jest.fn(),
+    deleteAddress: jest.fn(),
   });
   const mockFilesService = () => ({
     getUsers: jest.fn(),
+    uploadPublicFile: jest.fn(),
+    deletePublicFile: jest.fn(),
   });
+
   const mockAddressService = () => ({
-    getUsers: jest.fn(),
+    createAddressWithoutSave: jest.fn(),
+    createAddress: jest.fn(),
+    deleteAddress: jest.fn(),
+    updateAddress: jest.fn(),
+    updateAddressDirect: jest.fn(),
   });
 
   beforeEach(async () => {
@@ -52,6 +101,8 @@ describe('UserService', () => {
 
     userService = module.get<UserService>(UserService);
     userRepository = module.get<UserRepository>(UserRepository);
+    filesService = module.get<FilesService>(FilesService);
+    addressService = module.get<AddressService>(AddressService);
   });
 
   it('Should be defined', () => {
@@ -95,6 +146,91 @@ describe('UserService', () => {
       userRepository.getUserByEmail.mockResolvedValue('some user');
       const result = await userService.getUserByEmail('some email');
       expect(result).toEqual('some user');
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('Should delete user successfully', async () => {
+      userRepository.deleteUser.mockReturnValue({ deleted: true });
+      const result = await userService.deleteUser('some id');
+      expect(result).toEqual({ deleted: true });
+    });
+  });
+
+  describe('addAvatar', () => {
+    it('Should throw an error when user not found', async () => {
+      userRepository.getUserById.mockReturnValue(null);
+      try {
+        await userService.addAvatar('some id', {} as Buffer, 'some filename');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+    it('Should add avatar successfully', async () => {
+      userRepository.getUserById.mockReturnValue(oneUser);
+      filesService.uploadPublicFile.mockReturnValue(oneAvatar);
+      userRepository.updateAvatar.mockReturnValue(Promise.resolve());
+      const result = await userService.addAvatar(
+        'some id',
+        {} as Buffer,
+        'some filename',
+      );
+      expect(result).toEqual(oneAvatar);
+    });
+  });
+
+  describe('deleteAvatar', () => {
+    it('Should deleteAvatar successfully', async () => {
+      userRepository.getUserById.mockReturnValue(oneUserWithAvatar);
+      userRepository.updateAvatar.mockReturnValue(Promise.resolve());
+      filesService.deletePublicFile.mockReturnValue({ deleted: true });
+      const result = await userService.deleteAvatar('some id');
+      expect(result).toEqual({ deleted: true });
+    });
+  });
+
+  describe('userAddress', () => {
+    it('Should throw an error when user not found', async () => {
+      userRepository.getUserById.mockReturnValue(null);
+      addressService.createAddressWithoutSave.mockReturnValue();
+      try {
+        await userService.createUserAddress('some id', {} as CreateAddressDto);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+    describe('createAddress', () => {
+      it('Should create successfully address for user', async () => {
+        userRepository.getUserById.mockReturnValue(oneUser);
+        userRepository.createAddress.mockReturnValue(oneUser);
+        const result = await userService.createUserAddress(
+          'some userId',
+          oneAddress,
+        );
+        expect(result).toEqual(oneUser);
+      });
+    });
+
+    describe('updateAddress', () => {
+      it('Should create successfully address for user', async () => {
+        userRepository.getUserById.mockReturnValue(oneUser);
+        userRepository.updateAddress.mockReturnValue(oneUser);
+        const result = await userService.updateUserAddress(
+          'some userId',
+          oneAddress,
+        );
+        expect(result).toEqual(oneUser);
+      });
+    });
+    describe('deleteAddress', () => {
+      it('Should create successfully address for user', async () => {
+        userRepository.getUserById.mockReturnValue(oneUser);
+        userRepository.deleteAddress.mockReturnValue({ deleted: true });
+        const result = await userService.deleteUserAddress('some userId');
+        expect(result).toEqual({ deleted: true });
+      });
     });
   });
 });
